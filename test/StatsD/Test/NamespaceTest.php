14ce1ac840a11b095f87a6207dafab564363fb13
Marc Qualie <marc@marcqualie.com>
2013-06-26 22:04:41 +0100

    Added namespace unit tests

---
+++ test/StatsD/Test/NamespaceTest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace StatsD\Test;
+
+class NamespaceTest extends TestCase
+{
+
+    public function testNamespace()
+    {
+        $this->client->configure(array(
+            'host' => '127.0.0.1',
+            'port' => 8125,
+            'namespace' => 'test_namespace'
+        ));
+        $this->assertEquals($this->client->getNamespace(), 'test_namespace');
+    }
+
+
+    public function testNamespaceIncrement()
+    {
+        $this->client->configure(array(
+            'host' => '127.0.0.1',
+            'port' => 8125,
+            'namespace' => 'test_namespace'
+        ));
+        $this->client->increment('test_metric');
+        $this->assertEquals($this->client->getLastMessage(), 'test_namespace.test_metric:1|c');
+    }
+
+}
