101e9eeb5cc08ed2c74eeea8fd8d45adf44e2012
Marc Qualie <marc@marcqualie.com>
2013-07-13 22:58:14 +0100

    Added dencrement metric test

--- test/StatsD/Test/MetricsTest.php
+++ test/StatsD/Test/MetricsTest.php
@@ -15,4 +15,15 @@ class MetricsTest extends TestCase
         $this->assertEquals($this->client->getLastMessage(), 'test_metric:1|c');
     }
 
+
+    public function testDecrement()
+    {
+        $this->client->configure(array(
+            'host' => '127.0.0.1',
+            'port' => 8125,
+        ));
+        $this->client->decrement('test_metric');
+        $this->assertEquals($this->client->getLastMessage(), 'test_metric:-1|c');
+    }
+
 }
