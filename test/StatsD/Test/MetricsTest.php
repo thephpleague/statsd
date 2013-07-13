24e272266ba61898f7553ddd5c591ac0fdb7d91c
Marc Qualie <marc@marcqualie.com>
2013-07-13 23:01:24 +0100

    Added increment sample metric test

--- test/StatsD/Test/MetricsTest.php
+++ test/StatsD/Test/MetricsTest.php
@@ -16,6 +16,17 @@ class MetricsTest extends TestCase
     }
 
 
+    public function testIncrementSample()
+    {
+        $this->client->configure(array(
+            'host' => '127.0.0.1',
+            'port' => 8125,
+        ));
+        $this->client->increment('test_metric', 1, 0.5);
+        $this->assertEquals($this->client->getLastMessage(), 'test_metric:1|c|@0.5');
+    }
+
+
     public function testDecrement()
     {
         $this->client->configure(array(
