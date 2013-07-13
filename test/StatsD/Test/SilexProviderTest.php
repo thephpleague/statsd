9a9baa144f13d1eebd7e491878572f01f3b98841
Marc Qualie <marc@marcqualie.com>
2013-07-13 22:46:13 +0100

    Added namespace testing to Silex Service Provider

--- test/StatsD/Test/SilexProviderTest.php
+++ test/StatsD/Test/SilexProviderTest.php
@@ -11,16 +11,18 @@ class SilexProviderTest extends TestCase
         $app = new \Silex\Application();
         $app->register(new \Silex\Provider\StatsdServiceProvider(), array(
             'statsd.host' => 'localhost',
-            'statsd.port' => 7890
+            'statsd.port' => 7890,
+            'statsd.namespace' => 'test_namespace'
         ));
 
         // Make sure configuration is sorted
         $this->assertEquals($app['statsd']->getHost(), 'localhost');
         $this->assertEquals($app['statsd']->getPort(), 7890);
-        $this->assertEquals($app['statsd']->getNamespace(), '');
+        $this->assertEquals($app['statsd']->getNamespace(), 'test_namespace');
 
+        // Make sure messages are tracked properly
         $app['statsd']->increment('test_metric');
-        $this->assertEquals($app['statsd']->getLastMessage(), 'test_metric:1|c');
+        $this->assertEquals($app['statsd']->getLastMessage(), 'test_namespace.test_metric:1|c');
 
     }
 
