6f2aa1fc5d213622a988f46a405bfd6b94fbc9a7
Marc Qualie <marc@marcqualie.com>
2013-07-13 22:57:40 +0100

    Added increment metric test

---
+++ test/StatsD/Test/MetricsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace StatsD\Test;
+
+class MetricsTest extends TestCase
+{
+
+    public function testIncrement()
+    {
+        $this->client->configure(array(
+            'host' => '127.0.0.1',
+            'port' => 8125,
+        ));
+        $this->client->increment('test_metric');
+        $this->assertEquals($this->client->getLastMessage(), 'test_metric:1|c');
+    }
+
+}
