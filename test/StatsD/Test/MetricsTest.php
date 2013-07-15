7dce916d4b52cab39e0800cfbad1670087ec87ff
Marc Qualie <marc@marcqualie.com>
2013-07-15 19:19:35 +0100

    Removed un-needed test configuration

--- test/StatsD/Test/MetricsTest.php
+++ test/StatsD/Test/MetricsTest.php
@@ -7,10 +7,6 @@ class MetricsTest extends TestCase
 
     public function testIncrement()
     {
-        $this->client->configure(array(
-            'host' => '127.0.0.1',
-            'port' => 8125,
-        ));
         $this->client->increment('test_metric');
         $this->assertEquals($this->client->getLastMessage(), 'test_metric:1|c');
     }
@@ -18,10 +14,6 @@ class MetricsTest extends TestCase
 
     public function testIncrementDelta()
     {
-        $this->client->configure(array(
-            'host' => '127.0.0.1',
-            'port' => 8125,
-        ));
         $this->client->increment('test_metric', 2);
         $this->assertEquals($this->client->getLastMessage(), 'test_metric:2|c');
     }
@@ -29,10 +21,6 @@ class MetricsTest extends TestCase
 
     public function testIncrementSample()
     {
-        $this->client->configure(array(
-            'host' => '127.0.0.1',
-            'port' => 8125,
-        ));
         while ($this->client->getLastMessage() === '') {
             $this->client->increment('test_metric', 1,  0.75);
         }
@@ -42,10 +30,6 @@ class MetricsTest extends TestCase
 
     public function testDecrement()
     {
-        $this->client->configure(array(
-            'host' => '127.0.0.1',
-            'port' => 8125,
-        ));
         $this->client->decrement('test_metric');
         $this->assertEquals($this->client->getLastMessage(), 'test_metric:-1|c');
     }
