e31c13b5f7bf9ffe6e1afa085db309858518f91d
Marc Qualie <marc@marcqualie.com>
2013-07-15 19:20:48 +0100

    Changed phpunit assertion ordering

--- test/StatsD/Test/MetricsTest.php
+++ test/StatsD/Test/MetricsTest.php
@@ -8,14 +8,14 @@ class MetricsTest extends TestCase
     public function testIncrement()
     {
         $this->client->increment('test_metric');
-        $this->assertEquals($this->client->getLastMessage(), 'test_metric:1|c');
+        $this->assertEquals('test_metric:1|c', $this->client->getLastMessage());
     }
 
 
     public function testIncrementDelta()
     {
         $this->client->increment('test_metric', 2);
-        $this->assertEquals($this->client->getLastMessage(), 'test_metric:2|c');
+        $this->assertEquals('test_metric:2|c', $this->client->getLastMessage());
     }
 
 
@@ -24,14 +24,14 @@ class MetricsTest extends TestCase
         while ($this->client->getLastMessage() === '') {
             $this->client->increment('test_metric', 1,  0.75);
         }
-        $this->assertEquals($this->client->getLastMessage(), 'test_metric:1|c|@0.75');
+        $this->assertEquals('test_metric:1|c|@0.75', $this->client->getLastMessage());
     }
 
 
     public function testDecrement()
     {
         $this->client->decrement('test_metric');
-        $this->assertEquals($this->client->getLastMessage(), 'test_metric:-1|c');
+        $this->assertEquals('test_metric:-1|c', $this->client->getLastMessage());
     }
 
 }
