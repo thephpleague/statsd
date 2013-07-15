702d680ceb2c52a3bbe4db31989bee4df9975654
Marc Qualie <marc@marcqualie.com>
2013-07-15 19:27:34 +0100

    Added timing tests

---
+++ test/StatsD/Test/TimerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace StatsD\Test;
+
+class TimerTest extends TestCase
+{
+
+    public function testTiming()
+    {
+        $this->client->timing('test_metric', 123);
+        $this->assertEquals('test_metric:123|ms', $this->client->getLastMessage());
+    }
+
+
+    public function testFunctionTiming()
+    {
+        $this->client->time('test_metric', function () {
+            usleep(50000);
+        });
+        $this->assertRegExp('/test_metric:5[0-9]{1}\.[0-9]+\|ms/', $this->client->getLastMessage());
+    }
+
+}
