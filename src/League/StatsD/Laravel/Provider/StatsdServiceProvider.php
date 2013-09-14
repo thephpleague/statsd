16c9555e562f1d8ffeea7be6c4f289a654001851
Aran Wilkinson <aran3001@gmail.com>
2013-09-14 08:39:16 +0100

    Fixed code styling
    Signed-off-by: Aran Wilkinson <aran3001@gmail.com>

--- src/League/StatsD/Laravel/Provider/StatsdServiceProvider.php
+++ src/League/StatsD/Laravel/Provider/StatsdServiceProvider.php
@@ -10,14 +10,17 @@ use League\StatsD\Client as StatsdClient;
  *
  * @author Aran Wilkinson <aran@aranw.net>
  */
-class StatsdServiceProvider extends ServiceProvider {
+class StatsdServiceProvider extends ServiceProvider 
+{
 
     /**
      * Boot the service provider.
      *
      * @return void
      */
-    public function boot() { }
+    public function boot() 
+    {
+    }
 
     /**
      * Register the service provider.
@@ -36,24 +39,24 @@ class StatsdServiceProvider extends ServiceProvider {
      */
     protected function registerStatsD()
     {
-        $this->app['statsd'] = $this->app->share(function ($app) {
-                // Set Default host and port
-                $options = array();
-                if (isset($app['config']['statsd.host'])) {
-                    $options['host'] = $app['config']['statsd.host'];
-                }
-                if (isset($app['config']['statsd.port'])) {
-                    $options['port'] = $app['config']['statsd.port'];
-                }
-                if (isset($app['config']['statsd.namespace'])) {
-                    $options['namespace'] = $app['config']['statsd.namespace'];
-                }
-
-                // Create
-                $statsd = new StatsdClient();
-                $statsd->configure($options);
-                return $statsd;
+        $this->app['statsd'] = $this->app->share(function ($app) 
+        {
+            // Set Default host and port
+            $options = array();
+            if (isset($app['config']['statsd.host'])) {
+                $options['host'] = $app['config']['statsd.host'];
             }
-        );
+            if (isset($app['config']['statsd.port'])) {
+                $options['port'] = $app['config']['statsd.port'];
+            }
+            if (isset($app['config']['statsd.namespace'])) {
+                $options['namespace'] = $app['config']['statsd.namespace'];
+            }
+
+            // Create
+            $statsd = new StatsdClient();
+            $statsd->configure($options);
+            return $statsd;
+        });
     }
 }
