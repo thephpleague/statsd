efa5d2499825da8acec070e791069db0d37097ce
Richard Benson <me@richardbenson.co.uk>
2016-03-14 12:21:35 +0000

    And for Laravel 4

--- src/Laravel/Provider/StatsdServiceProvider.php
+++ src/Laravel/Provider/StatsdServiceProvider.php
@@ -53,6 +53,9 @@ class StatsdServiceProvider extends ServiceProvider
                 if (isset($app['config']['statsd.namespace'])) {
                     $options['namespace'] = $app['config']['statsd.namespace'];
                 }
+                if (isset($app['config']['statsd.throwConnectionExceptions'])) {
+                    $options['throwConnectionExceptions'] = $app['config']['statsd.throwConnectionExceptions'];
+                }
 
                 // Create
                 $statsd = new Statsd();
