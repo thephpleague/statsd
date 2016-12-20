82eb84740b119879446562508589e08f334980a2
Jildert Miedema <jildert@pimenta.nl>
2016-12-20 20:03:20 +0100

    use laravel 5 default for config

--- config/statsd.php
+++ config/statsd.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-	'host' => env('STATSD_HOST', '127.0.0.1'),
+    'host' => env('STATSD_HOST', '127.0.0.1'),
 
-	'port' => env('STATSD_PORT', 8125),
+    'port' => env('STATSD_PORT', 8125),
 
-	'namespace' => env('STATSD_NAMESPACE', ''),
+    'namespace' => env('STATSD_NAMESPACE', ''),
 
-	'throwConnectionExceptions' => true
+    'throwConnectionExceptions' => true
 ];
